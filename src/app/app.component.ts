114d5eed853482706566d3321e20b81aaca38337
ghchfolio <ghchfolio@gmail.com>
2022-01-08 11:55:37 -0600

    added data to loop thru

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface ICars {
+    year: number;
+    make: string;
+    model: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng12grid';
+    title = 'ng12grid';
+
+    cars: ICars[] = [
+        { 'year': 1999, 'make': 'Honda', 'model': 'Civic' },
+        { 'year': 2005, 'make': 'Jeep', 'model': 'Wrangler' },
+        { 'year': 2007, 'make': 'Subaru', 'model': 'WRX' },
+    ]
 }
