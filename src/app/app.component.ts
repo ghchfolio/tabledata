662c416879b61e117d4296e732e003bc566ed0f7
ghchfolio <ghchfolio@gmail.com>
2022-01-09 11:09:17 -0600

    added color to datasource for col testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ export interface ICars {
     year: number;
     make: string;
     model: string;
+    color: string;
     desc: string;
     ascSort?: boolean;
 }
@@ -18,9 +19,9 @@ export class AppComponent {
     title = 'ng12grid';
 
     cars: ICars[] = [
-        { id: 1, year: 1999, make: 'Honda', model: 'Civic', desc: 'aaaa dkjhaa ka aka kakj kja kak kak akkkhaakj aa akjak akj' },
-        { id: 2, year: 2015, make: 'Ford', model: 'Escort', desc: 'bbb ljlj bbbuiyb bljbljbiy b blj blj lbb lbb ljbbbb bljljljoib' },
-        { id: 3, year: 1960, make: 'Jeep', model: 'Wrangler', desc: 'ccc ci ciyicuc iictc cic cc iiyc iic icitc.' },
+        { id: 1, year: 1999, make: 'Honda', model: 'Civic', color: 'white', desc: 'aaaa dkjhaa ka aka kakj kja kak kak akkkhaakj aa akjak akj' },
+        { id: 2, year: 2015, make: 'Ford', model: 'Escort', color: 'red', desc: 'bbb ljlj bbbuiyb bljbljbiy b blj blj lbb lbb ljbbbb bljljljoib' },
+        { id: 3, year: 1960, make: 'Jeep', model: 'Wrangler', color: 'red', desc: 'ccc ci ciyicuc iictc cic cc iiyc iic icitc.' },
     ];
 
     sortState: any = {
